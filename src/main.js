862b0e6233276c4fbbfb7e58051f4d181878d5c1
QuayeWorks <113059255+QuayeWorks@users.noreply.github.com>
2025-11-26 23:58:02 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -62,8 +62,8 @@ const createScene = () => {
     // Chunked marching-cubes planet terrain
     // IMPORTANT: assign to the outer 'terrain' (no 'const' here)
     terrain = new ChunkedPlanetTerrain(scene, {
-        chunkCountX: 8,
-        chunkCountZ: 8,
+        chunkCountX: 32,
+        chunkCountZ: 32,
         baseChunkResolution: 8,
         dimY: 66600,
         cellSize: 1,
@@ -261,6 +261,7 @@ engine.runRenderLoop(() => {
 window.addEventListener("resize", () => {
     engine.resize();
 });
+
 
 
 
