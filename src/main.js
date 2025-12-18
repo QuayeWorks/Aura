4321f29e8df6faf36dcd61b97d3a68c1f073aa6d
QuayeWorks <113059255+QuayeWorks@users.noreply.github.com>
2025-12-18 01:16:28 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -304,8 +304,8 @@ function startGame() {
 
     if (!terrain) {
         terrain = new ChunkedPlanetTerrain(scene, {
-            chunkCountX: 32,
-            chunkCountZ: 32,
+            chunkCountX: 16,
+            chunkCountZ: 16,
             baseChunkResolution: 128,
             isoLevel: 0,
             radius: PLANET_RADIUS_UNITS
@@ -499,6 +499,7 @@ engine.runRenderLoop(() => {
 window.addEventListener("resize", () => {
     engine.resize();
 });
+
 
 
 
