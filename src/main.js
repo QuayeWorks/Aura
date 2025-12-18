5f4990d487b82a5b56ddc669d04b0c8c5a88932b
QuayeWorks <113059255+QuayeWorks@users.noreply.github.com>
2025-12-17 22:04:37 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -304,8 +304,8 @@ function startGame() {
 
     if (!terrain) {
         terrain = new ChunkedPlanetTerrain(scene, {
-            chunkCountX: 32,
-            chunkCountZ: 32,
+            chunkCountX: 16,
+            chunkCountZ: 16,
             baseChunkResolution: 128,
             isoLevel: 0,
             radius: PLANET_RADIUS_UNITS
@@ -317,10 +317,10 @@ function startGame() {
             // Create player on planet surface
             player = new PlanetPlayer(scene, terrain, {
                 planetRadius: PLANET_RADIUS_UNITS + 500,
-                walkSpeed: 4,
-                runSpeed: 22,
-                height: 5,
-                radius: 1
+                walkSpeed: 2,
+                runSpeed: 11,
+                height: 2,
+                radius: 0.5
             });
 
             if (mainCamera && player && player.mesh) {
@@ -499,6 +499,7 @@ engine.runRenderLoop(() => {
 window.addEventListener("resize", () => {
     engine.resize();
 });
+
 
 
 
