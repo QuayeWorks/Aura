cf3fb45064e09b20bf79771dc00036f9a2fb2741
QuayeWorks <113059255+QuayeWorks@users.noreply.github.com>
2026-01-01 03:10:14 -0500

    Update ChunkedPlanetTerrain.js

--- src/terrain/ChunkedPlanetTerrain.js
+++ src/terrain/ChunkedPlanetTerrain.js
@@ -6,7 +6,7 @@ import { resolveBiomeSettings, DEFAULT_BIOME_SETTINGS } from "./biomeSettings.js
 export class ChunkedPlanetTerrain {
     constructor(scene, options = {}) {
         this.scene = scene;
-        this.maxBuildDistance = 36000;
+        this.maxBuildDistance = 32400;
         this.seed = options.seed ?? 1337;
 
         // Legacy grid options kept for compatibility with callers / HUD
